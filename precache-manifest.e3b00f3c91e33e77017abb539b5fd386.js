d4efadf67f18d0b958bff653bab3322afbfd7598
samuelgfc13 <105825642+samuelgfc13@users.noreply.github.com>
2023-02-22 13:12:43 -0300

    Updates

---
+++ precache-manifest.e3b00f3c91e33e77017abb539b5fd386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "679c513bb2e570b0783e",
+    "url": "/atividadereact/static/css/main.a510c228.chunk.css"
+  },
+  {
+    "revision": "679c513bb2e570b0783e",
+    "url": "/atividadereact/static/js/main.c15a4cda.chunk.js"
+  },
+  {
+    "revision": "ad45cc008161358c44ff",
+    "url": "/atividadereact/static/js/runtime~main.195f014e.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/atividadereact/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "abb8821a23633196c3ba7ae926205042",
+    "url": "/atividadereact/static/media/Nunito-LightItalic.abb8821a.ttf"
+  },
+  {
+    "revision": "56360a4bac90fa5de4c15d8d91a1c73a",
+    "url": "/atividadereact/index.html"
+  }
+];
